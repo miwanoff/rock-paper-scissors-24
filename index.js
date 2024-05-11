fdc2537e42d743506289a3d4953dd9c766f4ec19
miwanoff <marina.iva.noffa@gmail.com>
2024-05-11 13:02:13 +0300

    Add static files

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require('path');
 
 const server = http.createServer(app);
 
+app.use(express.static(path.join(__dirname, 'client')));
+
 app.get("/healthcheck", (req, res) => {
-  res.send("<h1>RPS App running...</h1>");
+  res.send("<h2>RPS App running...</h2>");
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/index.html');
+});
+
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
