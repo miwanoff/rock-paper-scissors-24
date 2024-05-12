8bae381bc44c020abc4ee0726b5b80e32c775117
miwanoff <marina.iva.noffa@gmail.com>
2024-05-12 12:40:52 +0300

    Add navigator.clipboard button

--- client/client.js
+++ client/client.js
@@ -7,12 +7,32 @@ function сreateGame() {
 }
 
 function joinGame() {
-  roomUniqueId = document.getElementById('roomUniqueId').value;
+  roomUniqueId = document.getElementById("roomUniqueId").value;
   console.log(roomUniqueId);
-  socket.emit('joinGame', {roomUniqueId: roomUniqueId});
+  socket.emit("joinGame", { roomUniqueId: roomUniqueId });
 }
 
 socket.on("newGame", (data) => {
   roomUniqueId = data.roomUniqueId;
-});
+  document.getElementById("initial").style.display = "none";
+  document.getElementById("gamePlay").style.display = "block";
 
+  let copyButton = document.createElement("button");
+  copyButton.style.display = "block";
+  copyButton.innerText = "Copy Code";
+  copyButton.addEventListener("click", () => {
+    navigator.clipboard.writeText(roomUniqueId).then(
+      function () {
+        console.log("Async: Copying to clipboard was successful!");
+      },
+      function (err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  });
+
+  document.getElementById(
+    "waitingArea"
+  ).innerHTML = `Waiting for opponent, please share code ${roomUniqueId} to join`;
+  document.getElementById("waitingArea").appendChild(copyButton);
+});
